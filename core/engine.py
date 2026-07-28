09e72d505d4728e5141bd132bb639ea7ccb7beb8
Little_B <abidencube14@gmail.com>
2026-07-28 16:00:22 +0200

    Implement TradingEngine class with basic methods

--- core/engine.py
+++ core/engine.py
@@ -1,1 +1,27 @@
+"""
+==========================================
+Trading Engine
+Nova-Trader-BM
+==========================================
+"""
 
+from core.logger import info
+
+
+class TradingEngine:
+
+    def __init__(self):
+
+        info("Trading Engine Initialized")
+
+    def start(self):
+
+        info("Engine Started")
+
+    def analyse_market(self):
+
+        info("Analysing Market...")
+
+    def execute(self):
+
+        info("Execution Complete")
