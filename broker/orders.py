9fc2c8d2900c440db59940c40dbf0391a37e2f29
Little_B <abidencube14@gmail.com>
2026-07-28 16:27:25 +0200

    Implement OrderManager class with buy/sell methods

--- broker/orders.py
+++ broker/orders.py
@@ -1,1 +1,25 @@
+"""
+==========================================
+Order Manager
+Nova-Trader-BM
+==========================================
+"""
 
+
+class OrderManager:
+
+    def __init__(self):
+
+        self.active_orders = []
+
+    def buy(self, symbol, lot):
+
+        print(f"BUY {symbol} | Lot: {lot}")
+
+    def sell(self, symbol, lot):
+
+        print(f"SELL {symbol} | Lot: {lot}")
+
+    def close(self, ticket):
+
+        print(f"Closing Trade {ticket}")
