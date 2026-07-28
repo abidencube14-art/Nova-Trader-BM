f281a86b1dadfc48ecb6717c62b19d3c41655921
Little_B <abidencube14@gmail.com>
2026-07-28 15:41:42 +0200

    Add RSI calculation function in rsi.py

--- indicators/rsi.py
+++ indicators/rsi.py
@@ -1,1 +1,27 @@
+"""
+==========================================
+RSI Indicator
+Nova-Trader-BM
+==========================================
+"""
 
+import pandas as pd
+
+
+def calculate_rsi(data, period=14):
+
+    delta = data["close"].diff()
+
+    gain = delta.clip(lower=0)
+
+    loss = -delta.clip(upper=0)
+
+    avg_gain = gain.rolling(period).mean()
+
+    avg_loss = loss.rolling(period).mean()
+
+    rs = avg_gain / avg_loss
+
+    rsi = 100 - (100 / (1 + rs))
+
+    return rsi
