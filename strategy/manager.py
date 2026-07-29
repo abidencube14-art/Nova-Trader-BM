17b0180f7549b5863ef3c3afb8672022a2c30f43
Little_B <abidencube14@gmail.com>
2026-07-29 14:58:56 +0200

    Integrate IndicatorManager into StrategyManager

--- strategy/manager.py
+++ strategy/manager.py
@@ -5,6 +5,8 @@ Nova-Trader-BM
 ==========================================
 """
 
+from indicators.manager import IndicatorManager
+
 from strategy.entries import entry_signal
 
 
@@ -12,11 +14,15 @@ class StrategyManager:
 
     def __init__(self):
 
+        self.indicators = IndicatorManager()
+
         self.name = "EMA-RSI-MACD"
 
     def analyse(self, data):
 
-        return entry_signal(data)
+        values = self.indicators.analyse(data)
+
+        return entry_signal(values)
 
     def current_strategy(self):
 
