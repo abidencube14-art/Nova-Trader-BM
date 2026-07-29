ba4067a0efc352c6bdc1d2220a1f4e10e589d561
Little_B <abidencube14@gmail.com>
2026-07-29 16:15:51 +0200

    Create Event class with initialization
    Define the Event class with constructor for name and payload.

---
+++ events/event.py
@@ -0,0 +1,23 @@
+"""
+==========================================
+Event
+Nova-Trader-BM
+==========================================
+"""
+
+
+class Event:
+
+    def __init__(
+
+        self,
+
+        name,
+
+        payload=None
+
+    ):
+
+        self.name = name
+
+        self.payload = payload
