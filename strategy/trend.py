b36eee9ccce55a85e229539a800399d6b479cdf6
Little_B <abidencube14@gmail.com>
2026-07-29 14:56:20 +0200

    Refactor detect_trend function to use indicators

--- strategy/trend.py
+++ strategy/trend.py
@@ -5,19 +5,15 @@ Nova-Trader-BM
 ==========================================
 """
 
-from indicators.ema import calculate_ema
 
+def detect_trend(indicators):
 
-def detect_trend(data, fast_period=50, slow_period=200):
+    if indicators["ema50"] > indicators["ema200"]:
 
-    ema_fast = calculate_ema(data, fast_period)
-
-    ema_slow = calculate_ema(data, slow_period)
-
-    if ema_fast.iloc[-1] > ema_slow.iloc[-1]:
         return "BUY"
 
-    elif ema_fast.iloc[-1] < ema_slow.iloc[-1]:
+    if indicators["ema50"] < indicators["ema200"]:
+
         return "SELL"
 
     return "SIDEWAYS"
