6b79419adbc8955868cee22842e9fcc8dd98c2d4
Little_B <abidencube14@gmail.com>
2026-07-30 16:21:53 +0200

    Add PluginRegistry class for managing plugins

---
+++ plugins/registry.py
@@ -0,0 +1,24 @@
+"""
+==========================================
+Plugin Registry
+Nova-Trader-BM
+==========================================
+"""
+
+class PluginRegistry:
+
+    def __init__(self):
+
+        self.plugins = {}
+
+    def register(self, plugin):
+
+        self.plugins[plugin.name] = plugin
+
+    def get(self, name):
+
+        return self.plugins.get(name)
+
+    def all(self):
+
+        return list(self.plugins.values())
