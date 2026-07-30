5406bb3af1dc3c685badef72fc3a426877458d57
Little_B <abidencube14@gmail.com>
2026-07-30 15:12:19 +0200

    Add TradingAccount class with basic methods

---
+++ broker/account.py
@@ -0,0 +1,24 @@
+"""
+==========================================
+Account Information
+Nova-Trader-BM
+==========================================
+"""
+
+class TradingAccount:
+
+    def balance(self):
+
+        return 0.0
+
+    def equity(self):
+
+        return 0.0
+
+    def margin(self):
+
+        return 0.0
+
+    def free_margin(self):
+
+        return 0.0
