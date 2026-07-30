3614c6c62383a06ec145b549191631d01ced8dda
Little_B <abidencube14@gmail.com>
2026-07-30 16:08:56 +0200

    Create platforms.py for supported platforms
    Added a new file to list supported trading platforms.

---
+++ gateway/platforms.py
@@ -0,0 +1,18 @@
+"""
+==========================================
+Supported Platforms
+Nova-Trader-BM
+==========================================
+"""
+
+SUPPORTED_PLATFORMS = [
+
+    "SIMULATOR",
+
+    "MT5",
+
+    "MT4",
+
+    "CTRADER"
+
+]
