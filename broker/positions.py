6c83cf22fa19cf98c92e841bd46782f9259dc973
Little_B <abidencube14@gmail.com>
2026-07-30 15:18:40 +0200

    Create Positions class for managing open positions

---
+++ broker/positions.py
@@ -0,0 +1,16 @@
+"""
+==========================================
+Open Positions
+Nova-Trader-BM
+==========================================
+"""
+
+class Positions:
+
+    def all(self):
+
+        return []
+
+    def total(self):
+
+        return 0
