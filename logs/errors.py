d9f3b928f0d5c7425f66d526046c299db269d0a9
Little_B <abidencube14@gmail.com>
2026-08-05 16:29:11 +0200

    Add ErrorHandler class for error handling

---
+++ logs/errors.py
@@ -0,0 +1,20 @@
+"""
+==========================================
+Error Handler
+Nova-Trader-BM
+==========================================
+"""
+
+
+class ErrorHandler:
+
+    @staticmethod
+    def handle(error):
+
+        print()
+
+        print("Nova-Trader-BM Error")
+
+        print("--------------------")
+
+        print(error)
