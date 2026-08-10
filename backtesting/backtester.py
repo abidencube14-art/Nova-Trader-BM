a384a1b17a30f57f49112fa55ecb2594904ea0ab
Little_B <abidencube14@gmail.com>
2026-08-10 17:00:04 +0200

    Integrate SimulatorEngine and enhance run method
    Added SimulatorEngine to Backtester and updated run method to include index in results.

--- backtesting/backtester.py
+++ backtesting/backtester.py
@@ -5,6 +5,7 @@ Nova-Trader-BM
 ==========================================
 """
 
+from simulator.engine import SimulatorEngine
 from strategy.manager import StrategyManager
 
 
@@ -14,16 +15,28 @@ class Backtester:
 
         self.strategy = StrategyManager()
 
+        self.simulator = SimulatorEngine()
+
     def run(self, dataframe):
 
         results = []
 
+        if dataframe is None or len(dataframe) < 200:
+
+            return results
+
         for i in range(200, len(dataframe)):
 
-            data = dataframe.iloc[: i + 1]
+            data = dataframe.iloc[:i + 1]
 
             signal = self.strategy.analyse(data)
 
-            results.append(signal)
+            results.append({
+
+                "index": i,
+
+                "signal": signal
+
+            })
 
         return results
